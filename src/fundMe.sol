3f24a6e559f520ff85f29ed78899ea67fc818cce
Dharm Singh <dharmhsing@gmail.com>
2025-07-29 01:06:10 +0530

    practice test

---
+++ src/fundMe.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+import {PriceContverter} from "./prcieConverter.sol";
+contract FundMe{
+   error notOwner();
+   using  PriceContverter for uint256;
+   uint256 public constant  MINIMUM_USD=3e18; 
+   address[] public funders;
+   mapping (address=>uint256) public addressToAmountFunded;
+   function fund()public payable  {
+      // allow users to send money
+    //  have a  minimum $ sent
+
+         require(msg.value.getConversionRate() > MINIMUM_USD,"did not send enough ETH");
+         funders.push(msg.sender);
+         addressToAmountFunded[msg.sender]+=msg.value;
+   }  
+
+   address public immutable   i_owner; 
+
+   constructor(){
+      i_owner=msg.sender;
+   } 
+
+   function withdraw()public onlyOwner {
+       
+      for(uint256 funderIndex=0; funderIndex<funders.length; funderIndex++){
+         address funder=funders[funderIndex];
+         addressToAmountFunded[funder]=0;
+      }
+      funders=new address[](0);
+      // transfer
+      // send
+      // call
+      (bool callSuccess,)=payable(msg.sender).call{value:address(this).balance}("");
+      require(callSuccess,"call failed");
+   }
+
+   modifier onlyOwner(){
+      // require(msg.sender==owner,"not owner)
+          if (msg.sender!=i_owner){
+            revert notOwner() ;
+          }
+      _;
+   }
+
+   receive() external payable {
+      fund();
+    }  
+
+    fallback() external payable {
+      fund();
+     }
+
+    
+
+  
+}
---
+++ src/prcieConverter.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+import {AggregatorV3Interface} from "../lib/chainlink-brownie-contracts/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceContverter{
+
+   function getPrice() internal  view  returns (uint256) {
+    // Addresss sepolliatestnet  0x694AA1769357215DE4FAC081bf1f309aDC325306 
+    // Addresss zksync sepolliatestnet 0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF
+    AggregatorV3Interface priceFeed=AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF);
+    (,int256 price,,,)=priceFeed.latestRoundData();
+    return uint256(price*1e10);
+   } 
+   function getConversionRate(uint256 ethAmount) internal  view returns (uint256) {
+     uint256 ethPrice= getPrice();
+     uint256 ethAmountInUsd=(ethPrice*ethAmount)/1e18;
+     return ethAmountInUsd;
+
+
+   } 
+}
